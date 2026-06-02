f2594324a48447abbf9f981e9c52673d83f14323
Eliyahu Meir Rosenfeld <eliyahumeir12@gmail.com>
2026-06-02 17:42:04 +0300

    Add Express dependency and create a basic HTTP server
    - Updated package.json to include Express as a dependency.
    - Added a new file old_index.js that implements a simple HTTP server with routes for "/", "/users", and "/products".
    - The server responds with JSON messages and handles 404 errors.

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
+const express = require('express');
+const app = express();
 
-// const http = require("http") // מיישמים את המודול http
-const http_hadas = require('http')
+// get - delete - put - patch - post
+// הגדרת נתיב הבסיס (root) לשליחת תגובה מתאימה
+app.get('/',(req,res)=>{
+    res.json({
+        message: "Server Working"
+    });
+});
 
+// הגדרת נתיב /users לשליחת תגובה מתאימה
+app.get('/users', (req, res) => {
+    res.json({
+        message: "users route from express"
+    });
+});
 
-// יצירת שרת חדש
-const server_123 = http_hadas.createServer((req, res) => {
+// הגדרת נתיב /products לשליחת תגובה מתאימה
+app.get('/products', (req, res) => {
+    res.json({
+        message: "products route from express!"
+    });
+});
 
-    // הגדרת כותרת התגובה לסוג JSON
-    res.setHeader("Content-Type", "application/json")
-
-    // בדיקת הנתיב של הבקשה ושליחת תגובה מתאימה
-    if (req.url === "/") {
-        return res.end(JSON.stringify({
-            message: "Server Working"
-        }))
-    }
-   
-    // הוספת נתיבים נוספים לבדיקת הנתיב ולשליחת תגובות מתאימות
-    if (req.url === "/users") {
-        return res.end(JSON.stringify({
-            message: "users route"
-        }))
-    }
-
-    // הוספת נתיב נוסף לבדיקת הנתיב ולשליחת תגובה מתאימה
-    if (req.url === "/products") {
-        return res.end(JSON.stringify({
-            message: "products route 222222"
-        }))
-    }
-
-    // נטיב דיפולטי למקרה שהנתיב לא נמצא - הגדרת קוד סטטוס 404 ושליחת תגובה מתאימה
-    res.statusCode = 404
-
-    console.log("Route Not Found for", req.url)
-
-    res.end(JSON.stringify({
-        message: "Route Not Found"
-    }))
-})
+// הגדרת נתיב דיפולטי למקרה שהנתיב לא נמצא - הגדרת קוד סטטוס 404 ושליחת תגובה מתאימה
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Route Not Found from express"
+    });
+    console.log("Route Not Found for", req.url);
+});
 
 // הפעלת השרת והאזנה על פורט 3000
-server_123.listen(3000, () => {
-    console.log("Server running on port 3000")
-})
+app.listen(3001, () => {
+    console.log("Server running on port 3001 with Express");
+}); 
